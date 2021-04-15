ea917ada451cf221a418c48d5cba3a281dd7908e
Nakia Callaway <nik.callaway@outlook.com>
2021-04-15 13:08:42 -0700

    feat(pages and components)basic framework layout

---
+++ src/type.d.ts
@@ -0,0 +1,25 @@
+type critter = {
+    id: number,
+    num: string,
+    name: string,
+    img: string,
+    type: [string],
+    height: string,
+    weight: string,
+    candy: string,
+    candy_count: number,
+    egg: string,
+    spawn_chance: number,
+    avg_spawns: number,
+    spawn_time: string,
+    multipliers: [number],
+    weaknesses: [string],
+    next_evolution: [{
+        num: string,
+        name: string
+    }, {
+        num: string,
+        name: string
+        }]
+
+};
